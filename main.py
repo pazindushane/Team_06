d653237fc402c7e5be8a4ddd9a3170415b733482
SHASHINI PRABODHA <72292990+Shashini-Prabodha@users.noreply.github.com>
2022-05-07 14:26:42 +0530

    add flask

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask,request,render_template
+
+app=Flask(__name__)
+
+
+@app.route("/")
+def calc_volume(): #http://127.0.0.1:5000/calc?number_1=10&number_2=5
+    # area=request.args["name"]
+    # height=request.args["debit"]
+    return render_template("index.html")
+
+
+if __name__=="__main__":
+    app.run(debug=True)

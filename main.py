a9c0d87a2d9643ba3e9c90c606dcc8af095136bd
SHASHINI PRABODHA <72292990+Shashini-Prabodha@users.noreply.github.com>
2022-05-07 14:59:54 +0530

    add debit method in main

--- main.py
+++ main.py
@@ -2,13 +2,29 @@ from flask import Flask,request,render_template
 
 app=Flask(__name__)
 
+debit={}
 
 @app.route("/")
-def calc_volume(): #http://127.0.0.1:5000/calc?number_1=10&number_2=5
-    # area=request.args["name"]
-    # height=request.args["debit"]
+def view_index(): #http://127.0.0.1:5000/
+
     return render_template("index.html")
 
+
+
+@app.route("/debit")
+def add_debit(): #http://127.0.0.1:5000/debit?name=m&debit=19
+    name=request.form['name']
+    debit=request.form['debit']
+    # name=request.args["name"]
+    # debit=request.args["debit"]
+    response=open("templates/index.html").read()
+    f = open("DB.txt", "a")
+    record=name+" - "+debit
+    f.write(record)
+    f.close()
+
+    print(name,debit)
+    return response
 
 if __name__=="__main__":
     app.run(debug=True)
